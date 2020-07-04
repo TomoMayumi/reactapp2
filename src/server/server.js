9edcf60173d2ca4f602a7306caf05da2c0b6c408
TomoMayumi <TomoMayumi@gmail.com>
2020-07-04 15:54:51 +0900

    add config

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import path from 'path';
+import config from 'config';
 
 const app = express();
+
+const serverConfig = config.get('server');
 
 app.use(express.static(path.join('./', 'dist')));
 
@@ -13,6 +16,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join('./', 'dist', 'index.html'),{root:'./'})
 })
 
-app.listen(3000, ()=> {
-  console.log('server running');
+app.listen(serverConfig.port, ()=> {
+  console.log(`server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}`);
 })
